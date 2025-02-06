7dbf0908eddd15f4dbb39a6ee6833cd3c63efc12
Danish khan <89290282+Daneeshkhani@users.noreply.github.com>
2025-02-07 01:40:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+let display = document.getElementById("display");
+
+function toDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    display.value = eval(display.value);
+}
